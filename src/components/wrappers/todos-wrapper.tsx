d0a37f47aaaf565fefbf6acefeeea1ea8afd98ff
Ali Tamer <alitamer82.at@gmail.com>
2024-03-30 14:34:52 +0200

    updated packages and removed todo order

--- src/components/wrappers/todos-wrapper.tsx
+++ src/components/wrappers/todos-wrapper.tsx
@@ -4,7 +4,7 @@ import { TodoClientWrapper } from './todo-client-wrapper'
 
 export const TodosWrapper = async () => {
     const session = await getServerAuthSession()
-    const todosAsync = prisma.todo.findMany({
+    const todos = await prisma.todo.findMany({
         where: {
             userId: session?.user.id,
         },
@@ -12,27 +12,9 @@ export const TodosWrapper = async () => {
             createdAt: 'desc',
         },
     })
-    const todosOrderAsync = prisma.todoOrder.findUnique({
-        where: {
-            userId: session?.user.id,
-        },
-    })
 
-    const [todos, todosOrder] = await Promise.all([todosAsync, todosOrderAsync])
-
-    const orderedTodos = todos.sort((a, b) => {
-        const aIndex = todosOrder?.order.indexOf(a.id) || -1
-        const bIndex = todosOrder?.order.indexOf(b.id) || -1
-
-        if (aIndex === -1 || bIndex === -1) {
-            return 0
-        }
-
-        return aIndex - bIndex
-    })
-
-    const completedTodos = orderedTodos.filter((todo) => todo.completed)
-    const currentTodos = orderedTodos.filter((todo) => !todo.completed)
+    const completedTodos = todos.filter((todo) => todo.completed)
+    const currentTodos = todos.filter((todo) => !todo.completed)
 
     return (
         <TodoClientWrapper
